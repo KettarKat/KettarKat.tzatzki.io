a9dcd05f89af135b5cf4ce9676d3b3e0063a89b7
KettarKat <65594440+KettarKat@users.noreply.github.com>
2021-02-19 21:36:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('.product__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
+  });
+  document.body.addEventListener('scroll', function() {
+    var el = $('header');
+    var className = 'fixed';
+    if (document.body.scrollTop > 81 ) {
+    if (!el.hasClass(className))
+    el.addClass(className);
+    }
+    else {
+    if (el.hasClass(className))
+    el.removeClass(className);
+    }
+    });
+    
+    
+    var mixer = mixitup('.shop__inner-box');
+});
